b00955275d5f28560abbfe4268fcef4fc45a2a7e
Sirajum Monir Parvez <113641748+sirajum-smartone@users.noreply.github.com>
2024-04-30 14:07:14 +0600

    Created Hub method

--- SignalR/Hubs/UserHub.cs
+++ SignalR/Hubs/UserHub.cs
@@ -4,5 +4,12 @@ namespace SignalR.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            // Send the new total to all clients
+            await Clients.All.SendAsync("ReceiveMessage", TotalViews);
+        }
     }
 }
